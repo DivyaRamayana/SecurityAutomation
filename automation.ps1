8ae29bf339ae87144ecad0382ce7a733abc5fdb3
Divya R <Divya.R@rexnord.com>
2020-09-21 18:16:23 +0530

    Updated_multiple_domains

--- automation.ps1
+++ automation.ps1
@@ -1,3 +1,7 @@
-﻿$date="14-08-2020"
+﻿$domains = "rexnord.com","zurn.ad"
+$date="14-08-2020"
 #Get-ADUser -Filter * -Properties AccountExpirationDate | where {$_.AccountExpirationDate -ne $null -and $_.AccountExpirationDate.ToString() -like "$date*"}| Select sAMAccountName, AccountExpirationDate | export-csv "D:\ActiveDirectory\GetUserExpirydate.csv"
-Get-ADUser -Filter * -Server "rexnord.com","zurn.com" -Properties sAMAccountName,DisplayName, EmailAddress,AccountExpirationDate | where {$_.AccountExpirationDate -ne $null -and $_.AccountExpirationDate.ToString() -like "$date*"}| Select sAMAccountName,DisplayName, EmailAddress,AccountExpirationDate
+foreach($domain in $domains)
+{
+Get-ADUser -Filter * -Server $domain -Properties sAMAccountName,DisplayName, EmailAddress,AccountExpirationDate | where {$_.AccountExpirationDate -ne $null -and $_.AccountExpirationDate.ToString() -like "$date*"}| Select sAMAccountName,DisplayName, EmailAddress,AccountExpirationDate
+}
