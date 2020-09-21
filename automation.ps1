531a951ddb255cb3a22c3e3ec9689262b764ddae
Divya R <Divya.R@rexnord.com>
2020-09-21 15:07:58 +0530

    Add all my files

---
+++ automation.ps1
@@ -0,0 +1,3 @@
+﻿$date="14-08-2020"
+#Get-ADUser -Filter * -Properties AccountExpirationDate | where {$_.AccountExpirationDate -ne $null -and $_.AccountExpirationDate.ToString() -like "$date*"}| Select sAMAccountName, AccountExpirationDate | export-csv "D:\ActiveDirectory\GetUserExpirydate.csv"
+Get-ADUser -Filter * -Server "rexnord.com","zurn.com" -Properties sAMAccountName,DisplayName, EmailAddress,AccountExpirationDate | where {$_.AccountExpirationDate -ne $null -and $_.AccountExpirationDate.ToString() -like "$date*"}| Select sAMAccountName,DisplayName, EmailAddress,AccountExpirationDate
